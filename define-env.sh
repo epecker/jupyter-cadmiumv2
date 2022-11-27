25df04816efe1ccb43af06c7581d147393c84bec
epecker <ezepecker@gmail.com>
2022-11-27 13:12:59 -0500

    Added minor changes on the notebook for the presentation, images and a bash script to define all the needed environmental variables.

---
+++ define-env.sh
@@ -0,0 +1,12 @@
+export CADMIUMV2_HOME=$HOME/SED/demo-cadmiumv2
+export CADMIUMV2_DIR=$CADMIUMV2_HOME/cadmium_v2
+
+export CADMIUMV2_SRC=$CADMIUMV2_DIR/include/cadmium/core
+export CADMIUMV2_BLINKY_EXAMPLE=$CADMIUMV2_HOME/Blinky
+export CADMIUMV2_BLINKY_EXAMPLE_TOP=$CADMIUMV2_BLINKY_EXAMPLE/top_model
+export CADMIUMV2_COUNTER7SEG_EXAMPLE=$CADMIUMV2_HOME/Counter7seg
+export CADMIUMV2_COUNTER7SEG_EXAMPLE_TOP=$CADMIUMV2_BLINKY_EXAMPLE/top_model
+
+# URL for the repo
+export CADMIUMV2_TEST_REPO=https://github.com/epecker/cadmium_v2.git
+export BLINKY_EXAMPLE_REPO=https://github.com/epecker/Blinky.git
